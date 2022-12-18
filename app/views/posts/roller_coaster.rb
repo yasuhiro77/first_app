3108423a4f4d44bca9fb70faf1184a675405d239
yasuhiro77 <yasu082757@gmail.com>
2022-12-18 18:22:18 +0900

    first commit

---
+++ app/views/posts/roller_coaster.rb
@@ -0,0 +1,22 @@
+ride_count = 0
+friends = []
+
+3.times do
+  friend = {}
+  puts "お友達の名前は？"
+  friend[:name] = gets.chomp
+  puts "お友達の身長は？"
+  friend[:height] = gets.to_i
+  friends << friend
+end
+
+friends.each do |friend|
+ if friend[:height] >= 130
+  puts "#{friend[:name]}くんは乗車できます"
+  ride_count += 1
+ else
+  puts "#{friend[:name]}くんは乗車できません！"
+ end
+end
+
+puts "乗車するのは#{ride_count}人です"
